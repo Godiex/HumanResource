b8d91b5946355b496a03a6cc81b31aecef93e5db
Eduardo_Zequeira <eduardozequeira0@gmail.com>
2023-08-05 12:55:26 -0500

    se agrego location

---
+++ Entity/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Location
+    {
+        public int Id { set; get; }
+        public string StreetAddress { private set; get;} = string.Empty;
+        public string PostalCode { private set; get; } = string.Empty;
+        public string City { private set; get; } = string.Empty;
+        public string StateProvince { private set; get; } = string.Empty;
+
+        public Location(int id, string street_address, string postal_code, string city, string state_province)
+        {
+            Id = id;
+            StreetAddress = street_address;
+            PostalCode = postal_code;
+            City = city;
+            StateProvince = state_province;
+        }
+    }
+}

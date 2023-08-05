c70beaf63fe5b5948c688fad5b775c38fec94c7a
Eduardo_Zequeira <eduardozequeira0@gmail.com>
2023-08-05 13:03:41 -0500

    Se agrego location

--- Entity/Location.cs
+++ Entity/Location.cs
@@ -13,6 +13,7 @@ namespace Entity
         public string PostalCode { private set; get; } = string.Empty;
         public string City { private set; get; } = string.Empty;
         public string StateProvince { private set; get; } = string.Empty;
+        public Country Country { private set; get; }
 
         public Location(int id, string street_address, string postal_code, string city, string state_province)
         {
